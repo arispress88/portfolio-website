969fb3d91c69081485acbed01b6c0913a5438fd2
Arnold Rispress <arnold.rispress@gmail.com>
2023-10-06 12:57:16 -0400

    portfolio app complete!

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
 import github from "../../assets/icons/icons8-github.gif";
 import linkedin from "../../assets/icons/icons8-linkedin-circled.gif";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_1wsiaiw', 'template_duqpivd', form.current, 'edxHvB2P8XUAARW2t')
+          .then((result) => {
+              console.log(result.text);
+              e.target.reset();
+              alert("Your message has been sent successfully!");
+          }, (error) => {
+              console.log(error.text);
+          });
+        };
     return (
         <section id="contactPage">
             <div id="clients">
@@ -19,14 +33,18 @@ const Contact = () => {
             <div id="contact">
                 <h1 className="contactPageTitle">Contact Me</h1>
                 <span className="contactDesc">I'm currently available for freelance work or job opportunities. Please fill out the form below</span>
-                <form className="contactForm">
-                    <input type="text" className="name" placeholder="Your Name" />
-                    <input type="email" className="email" placeholder="Your Email" />
+                <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                    <input type="text" className="name" placeholder="Your Name" name="from_name" />
+                    <input type="email" className="email" placeholder="Your Email" name="from_email" />
                     <textarea className="msg" name="message" rows="5" placeholder="Your message" />
-                    <button className="submitBtn" type="submit" value="Send">Submit</button>
+                    <button type="submit" value="Send" className="submitBtn">Submit</button>
                     <div className="links">
-                        <img src={linkedin} alt="LinkedIn" className="link" />
-                        <img src={github} alt="Github" className="link" />
+                        <a href="https://www.linkedin.com/in/arnold-rispress" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedin} alt="LinkedIn" className="link" />
+                        </a>
+                        <a href="http://github.com/arispress88" target="_blank" rel="noopener noreferrer">
+                            <img src={github} alt="Github" className="link" />
+                        </a>
                     </div>
                 </form>
             </div>
